5713d7eaea77edf872c47abc6c523d86f5a4ff79
Bachle <dolly.bachle@capgemini.com>
2024-05-22 16:29:27 +0530

    first commit branch1

---
+++ src/Git/dempone/Newclass.java
@@ -0,0 +1,11 @@
+package Git.dempone;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to git Demo ");
+
+	}
+
+}
